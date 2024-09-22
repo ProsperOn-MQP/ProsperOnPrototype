5c6c4af2ef08bc171ee2e8fa6919d5d30b2d54a1
ahtruong1 <andyhtruong01@gmail.com>
2024-09-22 18:08:27 -0400

    db connection code refactor

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,19 +1,14 @@
-import express from "express";
+// Entry point for server
+import app from "./app.js";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./db/connection.js";
 
 // Load environmental variables from env file
 dotenv.config({path: '../../.env'});
-console.log()
 
-const app = express();
-
+// Set up: Connect to DB and start HTTP server
 connectToDatabase()
     .then(() => {
         app.listen(5000, () => console.log("Server Open and connected to database"));
     })
     .catch((err) => console.log(err));
-
-app.get("/hello", (req, res, next) => {
-    return res.send("Hello");
-});
