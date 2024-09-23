47ec5f946e245d6f7460e36d583bb44786403c5a
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-09-22 22:05:52 -0400

    local database to cloud

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,14 +1,17 @@
 // Entry point for server
+//import app from "./app.js";
 import app from "./app.js";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./db/connection.js";
 
 // Load environmental variables from env file
-dotenv.config({path: '../../.env'});
+dotenv.config({ path: "../../.env" });
 
 // Set up: Connect to DB and start HTTP server
 connectToDatabase()
-    .then(() => {
-        app.listen(5000, () => console.log("Server Open and connected to database"));
-    })
-    .catch((err) => console.log(err));
+  .then(() => {
+    app.listen(5000, () =>
+      console.log("Server Open and connected to database")
+    );
+  })
+  .catch((err) => console.log(err));
