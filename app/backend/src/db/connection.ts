f01a268c035daa9f33a90012f450b17ed0caf60a
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-09-30 19:14:40 -0400

    test with railway

--- app/backend/src/db/connection.ts
+++ app/backend/src/db/connection.ts
@@ -2,21 +2,21 @@
 import { connect, disconnect } from "mongoose";
 
 async function connectToDatabase() {
-    try {
-        await connect(process.env.DATABASE_URI);
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    await connect(
+      process.env.DATABASE_URI || "mongodb://localhost:27017/local"
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function disconnectFromDatabase() {
-    try {
-        await disconnect();
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    await disconnect();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export { connectToDatabase, disconnectFromDatabase }
+export { connectToDatabase, disconnectFromDatabase };
