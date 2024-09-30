aeb65acc20e5c5b50b7471a304c6d7f99b1d4359
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-09-30 18:07:18 -0400

    connection

--- app/backend/src/db/connection.ts
+++ app/backend/src/db/connection.ts
@@ -1,29 +1,23 @@
 import { connect, disconnect } from "mongoose";
 
-async function connectToDatabase() 
-{
-    try {
-        console.log(process.env.DATABASE_URI)
-        await connect(process.env.DATABASE_URI);
-    }
-    catch (error) 
-    {
-        console.log(error);
-        throw new Error("Cannot connect to database");
-    }
+async function connectToDatabase() {
+  try {
+    const dbUri = process.env.DATABASE_URI || "mongodb://localhost:27017/local";
+    console.log(dbUri);
+    await connect(dbUri);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Cannot connect to database");
+  }
 }
 
-async function disconnectFromDatabase() 
-{
-    try
-    {
-        await disconnect();
-    }
-    catch (error)
-    {
-        console.log(error);
-        throw new Error("Cannot disconnect from database");
-    }
+async function disconnectFromDatabase() {
+  try {
+    await disconnect();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Cannot disconnect from database");
+  }
 }
 
-export { connectToDatabase, disconnectFromDatabase }
+export { connectToDatabase, disconnectFromDatabase };
