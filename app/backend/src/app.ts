0f83c340c4e7248189a2eb2451cf9d15ee38a319
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-10-01 13:33:54 -0400

    frotnend

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -16,7 +16,7 @@ interface ChatLog {
 }
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Connected to app!");
+  res.json("Connected to app!");
 });
 
 const chatLogSchema = new mongoose.Schema<ChatLog>({
