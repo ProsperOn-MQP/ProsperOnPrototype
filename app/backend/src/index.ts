d54057a3efc3933980388ada1833990a1d22295c
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-10-01 11:32:21 -0400

    host

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -11,7 +11,7 @@ dotenv.config({ path: "../../.env" });
 // Set up: Connect to DB and start HTTP server
 connectToDatabase()
   .then(() => {
-    app.listen(port, () =>
+    app.listen(port, "0.0.0.0", () =>
       console.log(
         `Server Open and connected to database. Listening on port ${port}`
       )
