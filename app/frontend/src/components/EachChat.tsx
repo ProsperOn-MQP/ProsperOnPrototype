0013d11e15cb8a2fd0905c4d338e19b9563ffb0b
Perez0002 <op90210@gmail.com>
2024-10-01 15:03:10 -0400

    added faded message bgs

--- app/frontend/src/components/EachChat.tsx
+++ app/frontend/src/components/EachChat.tsx
@@ -5,39 +5,14 @@ interface EachChatProps {
   response: string;
 }
 
-/* g 
-style={{
-          wordWrap: "break-word",
-          maxWidth: "80%",
-          backgroundColor: "#d1e7dd",
-          padding: "8px",
-          borderRadius: "4px",
-          marginLeft: "auto",
-          right: "8px",
-        }}
-          
-        style={{
-          wordWrap: "break-word",
-          maxWidth: "80%",
-          backgroundColor: "#d2d5d8",
-          padding: "8px",
-          borderRadius: "4px",
-          marginTop: "4px",
-          marginBottom: "4px",
-        }}
-          
-        style={{ fontSize: "8pt", textAlign: "right" }}
-        style={{ fontSize: "8pt" }}style={{ textAlign: "left", marginBottom: "10px" }}
-*/
-
 const EachChat: React.FC<EachChatProps> = ({ message, response }) => {
   return (
     <div className="text-left mb-4">
-      <div className="break-words max-w-80 bg-wpi-red p-2 rounded ml-auto right-8">
+      <div className="break-words max-w-80 bg-wpi-red-faded p-2 rounded ml-auto right-8">
         <div className="text-xs font-semibold text-right">Username</div>{" "}
         {message}
       </div>
-      <div className="break-words max-w-80 bg-wpi-gray p-2 rounded my-4">
+      <div className="break-words max-w-80 bg-wpi-gray-faded p-2 rounded my-4">
         <div className="text-xs font-semibold text-left">Bot:</div> {response}
       </div>
     </div>
