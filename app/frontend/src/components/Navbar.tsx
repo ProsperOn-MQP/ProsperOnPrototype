338685f471e645fb5d864690aeddd59c73c254e5
Perez0002 <op90210@gmail.com>
2024-10-02 09:06:43 -0400

    popupcomp for all your popup needs

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import PopupComponent from "./PopupComponent";
 import HelpPage from "./HelpPage";
 import LoginPage from "./LoginPage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleQuestion,
   faArrowRightFromBracket,
-  faArrowRightToBracket
+  faArrowRightToBracket,
 } from "@fortawesome/free-solid-svg-icons";
-import Popup from "reactjs-popup";
 
 interface NavbarProps {
   isLoggedIn: boolean;
@@ -26,32 +26,14 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
 
   return (
     <header className="sticky bg-wpi-red inset-x-0 top-0 h-16 px-8 flex justify-end items-center space-x-4">
-      <Popup
+      <PopupComponent
         trigger={
           <button className="bg-black hover:bg-gray-700 text-white font-bold rounded focus:outline-none focus:shadow-outline">
             <FontAwesomeIcon icon={faCircleQuestion} /> Help
           </button>
         }
-        modal
-      >
-        {(close: () => void) => (
-          <div className="modal w-screen h-screen flex justify-center items-center">
-            <div className="absolute top-0 left-0 w-screen h-screen bg-black opacity-50 z-0"></div>
-            <div className="content border-2 border-black bg-white mx-4 p-8 rounded w-full md:w-1/2 lg:w-1/3 z-10">
-              <HelpPage />
-              <footer className="sticky bottom-0 h-16 grid justify-items-end items-center">
-                <button
-                  className="bg-black hover:bg-gray-700 text-white font-bold rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={() => close()}
-                >
-                  Close
-                </button>
-              </footer>
-            </div>
-          </div>
-        )}
-      </Popup>
+        pageComponent={<HelpPage />}
+      />
 
       {/*if logged in, show normal logout button*/}
       {isLoggedIn ? (
@@ -64,32 +46,14 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
           <FontAwesomeIcon icon={faArrowRightFromBracket} /> Logout
         </button>
       ) : (
-        <Popup
+        <PopupComponent
           trigger={
             <button className="bg-black hover:bg-gray-700 text-white font-bold rounded focus:outline-none focus:shadow-outline">
               <FontAwesomeIcon icon={faArrowRightToBracket} /> Login
             </button>
           }
-          modal
-        >
-          {(close: () => void) => (
-            <div className="modal w-screen h-screen flex justify-center items-center">
-              <div className="absolute top-0 left-0 w-screen h-screen bg-black opacity-50 z-0"></div>
-              <div className="content border-2 border-black bg-white mx-4 p-8 rounded w-full md:w-1/2 lg:w-1/3 z-10">
-                <LoginPage />
-                <footer className="sticky bottom-0 h-16 grid justify-items-end items-center">
-                  <button
-                    className="bg-black hover:bg-gray-700 text-white font-bold rounded focus:outline-none focus:shadow-outline"
-                    type="button"
-                    onClick={() => close()}
-                  >
-                    Close
-                  </button>
-                </footer>
-              </div>
-            </div>
-          )}
-        </Popup>
+          pageComponent={<LoginPage />}
+        />
       )}
     </header>
   );
