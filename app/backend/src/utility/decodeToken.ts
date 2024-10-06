1b4142bf887e6cc5756cab62784cf12a4b393cfc
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2024-10-06 19:14:14 -0400

    merge

--- app/backend/src/utility/decodeToken.ts
+++ app/backend/src/utility/decodeToken.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 // Returns payload from a given token
 function decodeToken(token) {
-    return jwt.verify(token, process.env.JWT_KEY);
+  return jwt.verify(token, process.env.JWT_KEY);
 }
 
 export default decodeToken;
