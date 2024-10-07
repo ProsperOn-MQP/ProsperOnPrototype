59c516fd858b53ccbd29e87e6c8bcc0c3e3fc799
Perez0002 <op90210@gmail.com>
2024-10-06 20:48:38 -0400

    tab name/logo, navbar 'special' logo ;)

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import {
   faArrowRightFromBracket,
   faArrowRightToBracket,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 interface NavbarProps {
   isLoggedIn: boolean;
@@ -16,6 +17,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, pageName }) => {
   const navigate = useNavigate();
+  const [isHovered, setIsHovered] = useState(false);
 
   //handle login
   const onLogout = async (e: any) => {
@@ -27,8 +29,20 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, pageName }) => {
 
   return (
     <header className="sticky bg-wpi-red inset-x-0 top-0 h-16 px-8 flex items-center relative">
+      <div
+        className="flex flex-row items-center space-x-4"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          className="max-h-16 px-3 py-2"
+          src={
+            isHovered ? "/src/assets/wwong2.jpg" : "/src/assets/wpi-goat.png"
+          }
+        />
+      </div>
       <h1 className="text-3xl text-white font-bold absolute left-1/2 transform -translate-x-1/2 mb-4 pt-2">
-        {pageName}
+        Welcome to GompeiOn AI
       </h1>
 
       <div className="ml-auto flex items-center space-x-4">
@@ -50,13 +64,15 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, pageName }) => {
             id="logoutButton"
             onClick={onLogout}
           >
-            <FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" /> <p>Logout</p>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" />{" "}
+            <p>Logout</p>
           </button>
         ) : (
           <PopupComponent
             trigger={
               <button className="bg-wpi-red hover:bg-red-800 px-3 py-2 flex-col text-white text-xs rounded focus:outline-none focus:shadow-outline">
-                <FontAwesomeIcon icon={faArrowRightToBracket} size="2x" /> <p>Login</p>
+                <FontAwesomeIcon icon={faArrowRightToBracket} size="2x" />{" "}
+                <p>Login</p>
               </button>
             }
             pageComponent={<LoginPage />}
